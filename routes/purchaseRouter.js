8a5d17cf0b795010c4c338c7c6adf0d6fb88dedb
Dika Wicaksono <dhkwck00@gmail.com>
2023-11-29 20:15:35 +0700

    create api for purchase

---
+++ routes/purchaseRouter.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+const purchaseController = require("../controllers/purchaseController");
+
+router
+  .route("/")
+  .get(purchaseController.getAllPurchase)
+  .post(purchaseController.createPurchase);
+
+router
+  .route("/:id")
+  .get(purchaseController.findPurchaseById)
+  .patch(purchaseController.updatePurchase)
+  .delete(purchaseController.deletePurchase);
+
+module.exports = router;

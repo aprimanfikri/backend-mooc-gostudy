ff98d09c85c55444b07456ad9dfb2448300b9551
Apriman Fikri Haikal <44273599+aprimanfikri@users.noreply.github.com>
2023-12-01 14:25:52 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const ApiError = require("../utils/apiError");
 const User = require("./userRouter");
 const Course = require("./courseRouter");
+const Chapter = require("./chapterRoutes");
 const Module = require("./moduleRouter");
 const Category = require("./categoryRouter");
 const Purchase = require("./purchaseRouter");
@@ -13,6 +14,7 @@ router.use(render);
 
 router.use("/api/v1/auth", User);
 router.use("/api/v1/course", Course);
+router.use("/api/v1/chapter", Chapter);
 router.use("/api/v1/module", Module);
 router.use("/api/v1/category", Category);
 router.use("/api/v1/purchase", Purchase);
@@ -20,6 +22,7 @@ router.use("/api/v1/purchase", Purchase);
 router.all("*", (req, res, next) => {
   res.render("error", {
     title: "Error Page",
+    });
 });
 
 module.exports = router;

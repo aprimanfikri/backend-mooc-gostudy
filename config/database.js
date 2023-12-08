68f061056d95dbde339dad1aa57aa3b31d5255ed
ImamTaufiqHermawan <imamtaufiq133@gmail.com>
2023-12-08 20:05:00 +0700

    change db config

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = {
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_test`,
+    database: `${process.env.DB_NAME}_development`,
     host: process.env.DB_HOST,
     dialect: "postgres",
     port: process.env.DB_PORT,

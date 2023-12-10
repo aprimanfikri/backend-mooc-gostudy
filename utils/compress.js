f37d86be9f44e0e73c230288cae287a3633f6867
aprimanfikri <aprmnfkr@gmail.com>
2023-12-11 00:56:43 +0700

    Add compress video and fix minor bugs

--- utils/compress.js
+++ utils/compress.js
@@ -3,23 +3,22 @@ const ffmpeg = require('fluent-ffmpeg');
 
 ffmpeg.setFfmpegPath(ffmpegStatic);
 
-const processVideo = async (inputPath, outputPath) =>
-  new Promise((resolve, reject) => {
-    ffmpeg()
-      .input(inputPath)
-      .size('50%')
-      .videoCodec('libx264')
-      .addOption('-x264opts', 'keyint=24:min-keyint=24:no-scenecut')
-      .outputOptions('-b:v', ['500k'])
-      .format('mp4')
-      .output(outputPath)
-      .on('end', () => {
-        resolve();
-      })
-      .on('error', (err) => {
-        reject(err);
-      })
-      .run();
-  });
+const processVideo = async (inputPath, outputPath) => new Promise((resolve, reject) => {
+  ffmpeg()
+    .input(inputPath)
+    .size('50%')
+    .videoCodec('libx264')
+    .addOption('-x264opts', 'keyint=24:min-keyint=24:no-scenecut')
+    .outputOptions('-b:v', ['500k'])
+    .format('mp4')
+    .output(outputPath)
+    .on('end', () => {
+      resolve();
+    })
+    .on('error', (err) => {
+      reject(err);
+    })
+    .run();
+});
 
 module.exports = { processVideo };

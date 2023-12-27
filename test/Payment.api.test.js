159a1a971776d7efb06dd438675b992a2483c009
ImamTaufiqHermawan <imamtaufiq133@gmail.com>
2023-12-28 01:45:18 +0700

    revert admin1

--- test/Payment.api.test.js
+++ test/Payment.api.test.js
@@ -20,7 +20,7 @@ describe("API create transaction", () => {
     tokenUser = login.body?.data?.token;
 
     const admin = {
-      email: "admin2@gmail.com",
+      email: "admin1@gmail.com",
       password: "admin1234",
     };
 

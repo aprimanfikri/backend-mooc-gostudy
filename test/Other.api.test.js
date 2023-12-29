de8674297e032cf73a298b20592be8e766300411
aprimanfikri <aprmnfkr@gmail.com>
2023-12-29 14:00:44 +0700

    Add youtube id in module

---
+++ test/Other.api.test.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const request = require('supertest');
+const { it, expect, describe } = require('@jest/globals');
+const app = require('../app');
+
+describe('should return 404', () => {
+  it('should return 404', async () => {
+    const res = await request(app).get('/api/v1/other');
+    expect(res.statusCode).toEqual(500);
+  });
+});
